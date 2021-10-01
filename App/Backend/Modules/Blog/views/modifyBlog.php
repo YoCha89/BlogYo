06e4538c17869b35b11002a28f7ae357a5df86f6
Yoann Chardel <ychardel@gmail.com>
2021-10-01 10:48:27 +0200

    blogpost controller and first backend views

---
+++ App/Backend/Modules/Blog/views/modifyBlog.php
@@ -0,0 +1,13 @@
+<form method="post" action="bootstrap.php?action=executePostBlog">
+	<div class="champ">
+		<label for="title">Titre :</label>
+		<input type="text" name="title" value="<?php echo $blogPost['title'] ?>"><br/>
+	</div>
+
+	<div class="champ">
+		<label for="content">Contenu :</label>
+		<input type="textarea" name="content" value="<?php echo $blogPost['content'] ?>"><br/>
+	</div>
+
+	<button type="submit" class="bouton">Mettre à jour</button>
+</form>
